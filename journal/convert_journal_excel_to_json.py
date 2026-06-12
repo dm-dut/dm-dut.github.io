13365da9e9e05162396045bc86a48251c4c6897d
desci-dut <zhen.zhang.dut@vip.163.com>
2026-06-12 17:38:50 +0800

    aa

--- journal/convert_journal_excel_to_json.py
+++ journal/convert_journal_excel_to_json.py
@@ -150,7 +150,7 @@ def main():
         journals.append({
             'category': category,
             'order': order_val,
-            'journal_old': journal,
+            'journal': journal,
             'url': url,
             'extra_links': parse_extra_links(extra),
             'note': note or ''
